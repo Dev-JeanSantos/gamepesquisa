de6f00ac36aab27972accac65dfee986cffbed31
Jean Santos <jeancsan@gmail.com>
2020-09-25 08:09:12 -0300

    Criação do Projeto

---
+++ backend/src/test/java/com/fourtk/gamepesquisa/GamepesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourtk.gamepesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamepesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
