9cf744e358e880c8eb65c5f5b0618891686a1451
Jean Santos <jeancsan@gmail.com>
2020-09-26 08:51:01 -0300

    Entitidades e Repositorios

---
+++ backend/src/main/java/com/fourtk/gamepesquisa/repositories/GenreRepository.java
@@ -0,0 +1,9 @@
+package com.fourtk.gamepesquisa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fourtk.gamepesquisa.entities.Genre;
+
+public interface GenreRepository extends JpaRepository<Genre, Long>{
+
+}
