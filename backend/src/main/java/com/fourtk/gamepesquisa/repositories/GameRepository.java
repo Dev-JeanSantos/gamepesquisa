3a525afed89afda4b32218b3f5e633495445c4b2
Jean Santos <jeancsan@gmail.com>
2020-09-26 09:42:47 -0300

    DTO,SERVICE

--- backend/src/main/java/com/fourtk/gamepesquisa/repositories/GameRepository.java
+++ backend/src/main/java/com/fourtk/gamepesquisa/repositories/GameRepository.java
@@ -1,9 +1,11 @@
 package com.fourtk.gamepesquisa.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.fourtk.gamepesquisa.entities.Game;
 
+@Repository
 public interface GameRepository extends JpaRepository<Game, Long>{
 
 }
--- backend/src/main/java/com/fourtk/gamepesquisa/repositories/GenreRepository.java
+++ backend/src/main/java/com/fourtk/gamepesquisa/repositories/GenreRepository.java
@@ -1,9 +1,11 @@
 package com.fourtk.gamepesquisa.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.fourtk.gamepesquisa.entities.Genre;
 
+@Repository
 public interface GenreRepository extends JpaRepository<Genre, Long>{
 
 }
